85e898daee52a9bb608fa988dc4f77b96257e0f2
vvt2605 <thanhvovan2605@gmail.com>
2024-04-24 16:24:31 +0700

    thanh create edges

--- physical_env/network/Network.py
+++ physical_env/network/Network.py
@@ -1,6 +1,12 @@
 import copy
 import numpy as np
 import Cluster
+import json
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.spatial import cKDTree
+from itertools import cycle
 
 class Network:
     def __init__(self, env, listNodes, baseStation, listTargets, max_time):
@@ -61,14 +67,70 @@ class Network:
             # [Cluster1,Cluster2 , . . . ]
         return None
     
+
+
+    
     def createEdges(self):
         # Input 
             # [Cluster1,Cluster2 , . . . ]
 
         # Output 
             # [(1,2),(3,2),(4,5) ,  . . .]
+            # funs for create edges
+        def calDistanceBS(cluster):
+            distance =  np.sqrt((cluster['centroid'][0] - 500)**2 + (cluster['centroid'][1] - 500)**2)
+            return distance
 
-        pass
+        def calDistanceCluster(cluster1, cluster2):
+            distance =  np.sqrt(np.sum((np.array(cluster1['centroid']) - np.array(cluster2['centroid']))**2))
+            return distance
+
+        def nearest_cluster_neighbor(cluster, listClusters, kdtree):
+            nearest_neighbor_idx = kdtree.query_ball_point(cluster['centroid'], calDistanceBS(cluster))
+            if len(nearest_neighbor_idx) > 1:
+                nearest_neighbor_idx.remove(listClusters.index(cluster))
+            if nearest_neighbor_idx:
+                nearest_neighbor = listClusters[nearest_neighbor_idx[0]]
+                return nearest_neighbor
+            else:
+                return None
+
+        cluster_centroids = [cluster['centroid'] for cluster in self.listClusters]
+        kdtree = cKDTree(cluster_centroids)
+
+        edges = []
+        for cluster in self.listClusters:
+            nearest_neighbor = nearest_cluster_neighbor(cluster, self.listClusters, kdtree)
+            if nearest_neighbor:
+                edges.append((cluster, nearest_neighbor))
+        
+        edge_colors = cycle(['g', 'b', 'y', 'c', 'm', 'k'])
+
+        # Vẽ các điểm trong các cluster và điểm centroid
+        cluster_colors = ['g', 'b', 'y', 'c', 'm', 'k']
+
+        for i, cluster_data in enumerate(self.listClusters):
+            color = cluster_colors[i % len(cluster_colors)]  
+            points = np.array([target["location"] for target in cluster_data["listTargets"]])
+            centroid = np.array(cluster_data["centroid"])
+            plt.scatter(points[:, 0], points[:, 1], color=color)
+            plt.scatter(centroid[0], centroid[1], color='red')
+
+        for edge, color in zip(edges, edge_colors):
+            x_values = [edge[0]['centroid'][0], edge[1]['centroid'][0]]
+            y_values = [edge[0]['centroid'][1], edge[1]['centroid'][1]]
+            plt.plot(x_values, y_values, color=color)
+        
+        plt.xlabel('X')
+        plt.ylabel('Y')
+        plt.show()
+        with open(edges.json, "w") as output_file:
+            json.dump(edges, output_file)
+        self.listEdges = edges
+        return edges
+
+
+       
 
     def createNodeInCluster(self):
         # Input 
