4254a5ade8b455dc05b792d36aa68e47e709590f
vvt2605 <thanhvovan2605@gmail.com>
2024-05-23 16:45:07 +0700

    update code clustering and create edges

--- physical_env/network/Network.py
+++ physical_env/network/Network.py
@@ -1,13 +1,15 @@
 
 import copy
 import numpy as np
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from scipy.spatial.distance import euclidean
 import math
 from scipy.signal import find_peaks
 import json
 import copy
+from sklearn.metrics import silhouette_score,silhouette_samples
+
 
 from scipy.spatial import cKDTree
 from itertools import cycle
@@ -69,55 +71,94 @@ class Network:
 
     def clustering(self):
 
+        def check_valid_cluster(clusterer, data, n_clusters, n_targets):
+            # mảng các label, mỗi label cluster là mảng các node
+            # print(n_clusters)
+            cluster_labels = clusterer.fit_predict(data)
+            centers = clusterer.cluster_centers_
+            cluster = [[] for _ in range(n_clusters)]
+            # print(cluster)
+            for index_node in range(0, n_targets):
+                # print(cluster_labels[index_node])
+                # print(data[index_node])
+                cluster[cluster_labels[index_node]].append(data[index_node])
+                # print(cluster)
+            for index_cluster in range(0, n_clusters):
+                for target in cluster[index_cluster]: 
+                    #
+                    if math.dist(target, centers[index_cluster]) > 140: 
+                        return False    
+            return True
+
         # Input : listTargets
-        listTargetLocation = []
+        list_targets_location = []
         for target in self.listTargets:
-            listTargetLocation.append(target.location)
-
-        # Elbow's method applying gradient rule to find number of clusters K
-        inertias = []
-        for k in range(1, len(listTargetLocation) + 1):
-            kmeans = KMeans(n_clusters=k, random_state=0)
-            kmeans.fit(listTargetLocation)
-            inertias.append(kmeans.inertia_)
-
-        gradient = np.gradient(inertias)
-        peaks, _ = find_peaks(gradient)
-        optimal_cluster = peaks[0] + 5
-        #optimal_cluster = 19
-
+            list_targets_location.append(target.location)
+        list_targets_location = np.array(list_targets_location)
+        # print(list_targets_location)
+        range_n_clusters = np.arange(2, int(len(list_targets_location)/2))
         
-        kmeans = KMeans(optimal_cluster)
-        kmeans.fit(listTargetLocation)
-        centers = kmeans.cluster_centers_
+        max_silhouette_score = 0
+        n_clusters_optimal = 0
 
-        # temporary drawing process
-        x = []
-        y = []
-        for targetLocation in listTargetLocation:
-            x.append(targetLocation[0])
-            y.append(targetLocation[1])
+        for n_clusters in range_n_clusters:
+            # clusterer = KMeans(n_clusters=n_clusters, random_state=10, n_init="auto", init="k-means++",  algorithm='elkan')
+            clusterer = KMeans(n_clusters=n_clusters, init="k-means++",  algorithm='elkan')
 
-        # plt.scatter(x, y, c=kmeans.labels_)
-        # plt.scatter(centers[:, 0], centers[:, 1], c='red', marker='o', s=20)  # Vẽ các tâm của mỗi cluster
-        # plt.show()
+            cluster_labels = clusterer.fit_predict(list_targets_location)
+            silhouette_avg = silhouette_score(list_targets_location, cluster_labels)
 
-        # Output : [Cluster1,Cluster2 , . . . ]
+            # print(
+            #     "For n_clusters_optimal =",
+            #     n_clusters,
+            #     "The max silhouette",
+            #     max_silhouette_score,
+            #     "The optimal K",
+            #     n_clusters_optimal,
+            #     "The average silhouette_score is :",
+            #     silhouette_avg,
+            # )
+
+            if check_valid_cluster(clusterer, list_targets_location, n_clusters, len(list_targets_location)) is True:
+                if max_silhouette_score < silhouette_avg:
+                    max_silhouette_score = silhouette_avg
+                    n_clusters_optimal = n_clusters
+
+        clusterer = KMeans(n_clusters=n_clusters, init="k-means++",  algorithm='elkan')
+        # clusterer = KMeans(n_clusters=n_clusters_optimal, random_state=None,  n_init="auto", init="k-means++", algorithm='lloyd')
+
+        cluster_labels = clusterer.fit_predict(list_targets_location)
+        silhouette_avg = silhouette_score(list_targets_location, cluster_labels)
+        self.n_clusters_optimal = n_clusters_optimal
+    
+        # Labeling the clusters
+        centers = clusterer.cluster_centers_
+
         clusters = []
-        labels = kmeans.labels_
         
-        for i in range(0, optimal_cluster):
+        for i in range(0, n_clusters_optimal):
             listTargetsInCluster = []
-            for j in range(0, len(listTargetLocation)):
-                if labels[j] == i:
+            for j in range(0, len(list_targets_location)):
+                if cluster_labels[j] == i:
                     listTargetsInCluster.append(self.listTargets[j])
             cluster = Cluster(i, listTargetsInCluster, centers[i])
             clusters.append(cluster)
 
+        # Chuyển đổi danh sách thành danh sách các từ điển
+        json_data = [convert_cluster_to_dict(cluster) for cluster in clusters]
+        # Chuyển đổi danh sách thành chuỗi JSON
+        json_string = json.dumps(json_data, indent=4)
+        with open("clusters.json", "w") as json_file:
+            json.dump(json_data, json_file, indent=4)
 
         return clusters
-    
+
     def createEdges(self):
+        # xét từng centroid
+            # tạo ra 3 danh sách s1 s2 s3
+            # giao của 3 danh sách là 1 mảng chứa các id của cluster(danh sách các output )
+            # diểm 
+
         # Input 
             # [Cluster1,Cluster2 , . . . ]
 
@@ -125,55 +166,178 @@ class Network:
             # [(1,2),(3,2),(4,5) ,  . . .]
             # [(cluster1, cluster2), (cluster, base station),  . . . ]
             # funs for create edges
-        def calDistanceBS(cluster):
-            distance =  np.sqrt((cluster.centroid[0] - 500)**2 + (cluster.centroid[1] - 500)**2)
-            return distance
+        # def calDistanceBS(cluster):
+        #     distance =  np.sqrt((cluster.centroid[0] - 500)**2 + (cluster.centroid[1] - 500)**2)
+        #     return distance
 
-        def calDistanceCluster(cluster1, cluster2):
-            distance =  np.sqrt(np.sum((np.array(cluster1.centroid) - np.array(cluster2.centroid))**2))
-            return distance
+        # def calDistanceCluster(cluster1, cluster2):
+        #     distance =  np.sqrt(np.sum((np.array(cluster1.centroid) - np.array(cluster2.centroid))**2))
+        #     return distance
 
-        # def nearest_cluster_neighbor(cluster, listClusters, kdtree):
-        #     nearest_neighbor_idx = kdtree.query_ball_point(cluster.centroid, calDistanceBS(cluster))
-        #     if len(nearest_neighbor_idx) > 1:
-        #         nearest_neighbor_idx.remove(listClusters.index(cluster))
-        #     if nearest_neighbor_idx:
-        #         nearest_neighbor = listClusters[nearest_neighbor_idx[0]]
-        #         return nearest_neighbor
+        # def nearest_cluster_neighbor(cluster):
+        #     min_distance_cluster = float('inf')
+        #     nearest_neighbor = None
+        #     for other_cluster in self.listClusters:
+        #         if cluster != other_cluster and calDistanceBS(other_cluster) < calDistanceBS(cluster):
+        #             distance = calDistanceCluster(cluster, other_cluster)
+        #             if distance < min_distance_cluster:
+        #                 min_distance_cluster = distance
+        #                 nearest_neighbor = other_cluster
+        #     return nearest_neighbor
+
+        # cluster_centroids = [cluster.centroid for cluster in self.listClusters]
+        # kdtree = cKDTree(cluster_centroids)
+
+        # edges = []
+        # for cluster in self.listClusters:
+        #     nearest_neighbor = nearest_cluster_neighbor(cluster)
+        #     if nearest_neighbor:
+        #         if calDistanceCluster(cluster,nearest_neighbor) <  calDistanceBS(cluster):
+        #             edges.append((cluster, nearest_neighbor))
+        #         else:
+        #             edges.append((cluster, self.baseStation))
         #     else:
-        #         return None
-            
+        #         edges.append((cluster,self.baseStation))
 
-        def nearest_cluster_neighbor(cluster):
-            min_distance_cluster = float('inf')
-            nearest_neighbor = None
-            for other_cluster in self.listClusters:
-                if cluster != other_cluster and calDistanceBS(other_cluster) < calDistanceBS(cluster):
-                    distance = calDistanceCluster(cluster, other_cluster)
-                    if distance < min_distance_cluster:
-                        min_distance_cluster = distance
-                        nearest_neighbor = other_cluster
-            return nearest_neighbor
+        def cal_distance(point1, point2):
+            return np.sqrt((point1[1]-point2[1])**2+(point1[2]-point2[2])**2)
 
-        cluster_centroids = [cluster.centroid for cluster in self.listClusters]
-        kdtree = cKDTree(cluster_centroids)
+        def list_nearest_centroid(centroid_x,centroid_y, list_centroid):
+            sorted_points = sorted(list_centroid, key=lambda x: ((x[1] - centroid_x) ** 2 + (x[2] - centroid_y) ** 2) ** 0.5)
+            # Chọn ra các id của các điểm gần nhất
+            closest_point_ids = [point[0] for point in sorted_points]
 
+            # print(closest_point_ids)
+            return closest_point_ids
+        
+        def angle_between_points(point1, point2):
+            A= np.array([point1[1],point1[2]])
+            B= np.array([point2[1],point2[2]])
+            C= np.array(self.baseStation.location)
+            vector_AB = B - A
+            vector_AC = C - A
+            angle_BAC = np.arctan2(np.linalg.det([vector_AB, vector_AC]), np.dot(vector_AB, vector_AC))
+            angle_BAC_deg = np.degrees(angle_BAC)
+            return angle_BAC_deg
+
+        list_id_centroid_cluster = []
+        bs = [-1,500,500]
         edges = []
+        edges_id = []
+        list_common_elements = []
         for cluster in self.listClusters:
-            nearest_neighbor = nearest_cluster_neighbor(cluster)
-            if nearest_neighbor:
-                if calDistanceCluster(cluster,nearest_neighbor) <  calDistanceBS(cluster):
-                    edges.append((cluster, nearest_neighbor))
-                else:
-                    edges.append((cluster, self.baseStation))
+            list_id_centroid_cluster.append((cluster.id,cluster.centroid[0],cluster.centroid[1]))
+        # print(list_id_centroid_cluster)
+        # sorted_points = sorted(list_id_centroid_cluster, key=lambda point: cal_distance(point,bs))
+        sorted_points = sorted(list_id_centroid_cluster, key=lambda point: cal_distance(point, bs), reverse=True)
+        sorted_ids_distance = [point[0] for point in sorted_points]
+        
+        for point in sorted_points:
+            list_angle_satisfy= []
+            list_point_distance_satisfy = []
+            sorted_nearest_point = list_nearest_centroid(point[1],point[2],list_id_centroid_cluster)
+
+            for other_point in list_id_centroid_cluster:
+                if point != other_point and angle_between_points(point,other_point)<= 45 and angle_between_points(point,other_point)>= -45:
+                    list_angle_satisfy.append(other_point[0])
+
+            for other_point in list_id_centroid_cluster:
+                if point != other_point:
+                    if cal_distance(point,other_point)< cal_distance(point,bs) and cal_distance(point,bs) > cal_distance(other_point,bs):
+                        list_point_distance_satisfy.append(other_point[0])
+            
+             
+            common_elements = []
+            for id in sorted_nearest_point:
+                if id in list_angle_satisfy and id in list_point_distance_satisfy:
+                    common_elements.append(id)
+            list_common_elements.append([point[0],common_elements])
+
+            #  phần trên tạo cạnh với một đầu ra 
+            # if common_elements:
+            #     for cluster in self.listClusters:
+            #         if cluster.id == point[0]:
+            #             cluster1 = cluster
+            #     for cluster in self.listClusters:
+            #         if cluster.id == common_elements[0]:
+            #             # edges.append((cluster1,cluster))
+            #             edges_id.append([cluster1.id,cluster.id])
+            #     common_elements.pop(0)
+            # else: 
+            #     for cluster in self.listClusters:
+            #         if cluster.id == point[0]:
+            #             # edges.append((cluster,self.baseStation))
+            #             edges_id.append([cluster.id,-1])
+            # common_elements_2.append((id,common_elements))
+        
+        print(list_common_elements)
+
+        # Số target mà cụm phải tải ban đầu
+        num_targets_clusters=[]
+        for id in sorted_ids_distance:
+            for cluster in self.listClusters:
+                if id == cluster.id:
+                    num_targets_clusters.append([cluster.id,len(cluster.listTargets)])
+        print(num_targets_clusters)
+
+        # Chọn cạnh
+        for point in list_common_elements:
+            id =point[0]
+            common_points = point[1]
+            for targets_cluster1 in num_targets_clusters:
+                if id == targets_cluster1[0]:
+                    num_targets_point = targets_cluster1[1]
+             
+            if common_points:
+                num = 0
+                found = False
+                for common_point in common_points:
+                    for targets_cluster in num_targets_clusters:
+                        if targets_cluster[0]==common_point:
+                            if num_targets_point + targets_cluster[1] < 40:
+                                edges_id.append([id,common_point])
+                                targets_cluster[1]=targets_cluster[1]+num_targets_point
+                                found=True
+                                break
+                    if found:
+                        break
+                if not found:
+                    edges_id.append([id,-1]) 
             else:
-                edges.append((cluster,self.baseStation))
+                for cluster in self.listClusters:
+                    if cluster.id == id:
+                        edges_id.append((id,-1))
+
+
+
+
+        for targets_cluster in num_targets_clusters:
+            for edge in edges_id:
+                if targets_cluster[0] == edge[1]:
+                    for add_targets in num_targets_clusters:
+                        if add_targets[0] == edge[0]:
+                            targets_cluster[1] = targets_cluster[1]+add_targets[1]
+                            break
+                    
+        edges = []
+        for edge in edges_id:
+            for cluster in self.listClusters:
+                if cluster.id == edge[0]:
+                    cluster1 = cluster  
+                if cluster.id == edge[1]:
+                    cluster2 = cluster  
+                if edge[1] == -1:
+                    cluster2 = self.baseStation 
+            edges.append((cluster1,cluster2))
+        print(num_targets_clusters)
+    
+        # Vẽ 
         edge_colors = cycle([ 'g', 'b', 'y', 'c', 'm', 'k'])
 
         cluster_colors = ['g', 'b', 'y', 'c', 'm', 'pink', 'orange', 'purple', 
                         'brown', 'olive', 'teal', 'navy', 'maroon', 'lime', 'aqua', 'fuchsia',
                         'indigo', 'gold']
-        # plt.figure(figsize=(10, 8))
+        plt.figure(figsize=(10, 10))
         for i in range(len(self.listClusters)):
             cluster = self.listClusters[i]
             color = cluster_colors[i % len(cluster_colors)]  
@@ -186,31 +350,34 @@ class Network:
             centroid_x = centroid[0]
             centroid_y = centroid[1]
             # Vẽ các điểm trong cluster (trừ điểm centroid)
-            # plt.scatter(x_points, y_points, color=color)
-            # # Vẽ điểm centroid
-            # plt.scatter(centroid_x, centroid_y, color='red')
+            plt.scatter(x_points, y_points, color=color)
+            # Vẽ điểm centroid
+            plt.scatter(centroid_x, centroid_y, color='red')
         # Vẽ các cạnh giữa các cluster
         for edge, color in zip(edges, edge_colors):
 
             if edge[1] is self.baseStation:
                 x_values = [edge[0].centroid[0], 500]
                 y_values = [edge[0].centroid[1], 500]
-                # plt.plot(x_values, y_values, color=color)
+                plt.plot(x_values, y_values, color=color)
             else:
                 # Trích xuất tọa độ của các điểm trong cạnh
                 x_values = [edge[0].centroid[0], edge[1].centroid[0]]
                 y_values = [edge[0].centroid[1], edge[1].centroid[1]]
-                # plt.plot(x_values, y_values, color=color)
-        # plt.scatter(500, 500, color='red', marker='*', s=300, label='Base Station')
+                plt.plot(x_values, y_values, color=color)
+        plt.scatter(500, 500, color='red', marker='*', s=300, label='Base Station')
         
-        # plt.xlabel('X')
-        # plt.ylabel('Y')
-        # plt.show()
+        plt.xlabel('X')
+        plt.ylabel('Y')
+        plt.show()
         
         # with open(edges.json, "w") as output_file:
         #     json.dump(edges, output_file)
+
+
         return edges
-    
+        pass
+
     def createNodeInCluster(self):
         # Input 
             # self.listClusters, self.listEdges
@@ -382,7 +549,10 @@ class Network:
                     ListRelayNode.append(RelayNode([U[0],U[1]],ID,self.phy,u,v))
 
         return ListRelayNode
+    
 
+
+    
     def setLevels(self):
         for node in self.listNodes:
             node.level = -1
@@ -446,4 +616,9 @@ class Network:
                 tmp += 1
         return tmp
 
-
+def convert_cluster_to_dict(cluster):
+        return {
+            'cluster_id': cluster.id,
+            'listTargets': [target.__dict__ for target in cluster.listTargets],
+            'centroid': cluster.centroid.tolist()  # Chuyển mảng numpy thành danh sách Python
+        }
